a6a6f2ca5ca4e1e711372f052b93f92150063f5d
artem <artem@DESKTOP-J9GO0U3>
2023-11-28 22:25:19 +0200

    Добавьте файлы проекта.

---
+++ Lab4/Point.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Lab4
+{
+    interface IUnit
+    {
+        double X { get; set; }
+        double Y { get; set; }
+    }
+    public class Point : IUnit
+    {
+        double x;
+        double y;
+
+        public Point()
+        {
+            var rnd = new Random();
+            x = rnd.Next(0, 5);
+            y = rnd.Next(0, 5);
+        }
+
+        public Point(double x1, double y1)
+        {
+            x = x1;
+            y = y1;
+        }
+
+        public double X { get { return x; } set { x = value; } }
+        public double Y { get { return y;} set { y = value; } }
+
+        public void movePoint(int xm, int ym)
+        {
+            x = x + xm;
+            y = y + ym;
+        }
+    }
+
+    public class Square : Point
+    {
+        Point a;
+        Point b;
+        Point c;
+        Point d;
+
+        public Square(double x, double y)
+        {
+            a = new Point(x, y);
+            b = new Point(x, y + 5);
+            c = new Point(x + 5, y + 5);
+            d = new Point(x + 5, y);
+        }
+
+        public void moveSquare(int xm, int ym)
+        {
+            a.movePoint(xm, ym);
+            b.movePoint(xm, ym);
+            c.movePoint(xm, ym);
+            d.movePoint(xm, ym);
+        }
+
+        public double AX { get { return a.X; } set { a.X = value; } }
+        public double AY { get { return a.Y;} set { a.Y = value; } }
+        public double BX { get { return b.X;} set { b.X = value; } }
+        public double BY { get { return b.Y;} set { b.Y = value; } }
+        public double CX { get { return c.X;} set { c.X = value; } }
+        public double CY { get { return c.Y;} set { c.Y = value; } }
+        public double DX { get { return d.X; } set { d.X = value; } }
+        public double DY { get { return d.Y; } set { d.Y = value; } }
+    }
+
+    public class Rectangle : Point
+    {
+        Point a;
+        Point b;
+        Point c;
+        Point d;
+
+        public Rectangle(double x, double y)
+        {
+            a = new Point(x, y);
+            b = new Point(x, y + 5);
+            c = new Point(x + 10, y + 5);
+            d = new Point(x + 10, y);
+        }
+
+        public void moveRectangle(int xm, int ym)
+        {
+            a.movePoint(xm, ym);
+            b.movePoint(xm, ym);
+            c.movePoint(xm, ym);
+            d.movePoint(xm, ym);
+        }
+
+        public double AX { get { return a.X; } set { a.X = value; } }
+        public double AY { get { return a.Y; } set { a.Y = value; } }
+        public double BX { get { return b.X; } set { b.X = value; } }
+        public double BY { get { return b.Y; } set { b.Y = value; } }
+        public double CX { get { return c.X; } set { c.X = value; } }
+        public double CY { get { return c.Y; } set { c.Y = value; } }
+        public double DX { get { return d.X; } set { d.X = value; } }
+        public double DY { get { return d.Y; } set { d.Y = value; } }
+    }
+
+    public class Romb : Point
+    {
+        Point a;
+        Point b;
+        Point c;
+        Point d;
+
+        public Romb(double x, double y)
+        {
+            a = new Point(x, y);
+            b = new Point(x + 1.25, y + 5);
+            c = new Point(x + 6.25, y + 5);
+            d = new Point(x + 5, y);
+        }
+
+        public void moveRectangle(int xm, int ym)
+        {
+            a.movePoint(xm, ym);
+            b.movePoint(xm, ym);
+            c.movePoint(xm, ym);
+            d.movePoint(xm, ym);
+        }
+
+        public double AX { get { return a.X; } set { a.X = value; } }
+        public double AY { get { return a.Y; } set { a.Y = value; } }
+        public double BX { get { return b.X; } set { b.X = value; } }
+        public double BY { get { return b.Y; } set { b.Y = value; } }
+        public double CX { get { return c.X; } set { c.X = value; } }
+        public double CY { get { return c.Y; } set { c.Y = value; } }
+        public double DX { get { return d.X; } set { d.X = value; } }
+        public double DY { get { return d.Y; } set { d.Y = value; } }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,176 @@
+﻿using Lab4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class PointTests
+    {
+        [TestMethod]
+        public void CreatePoint()
+        {
+            var a = new Point();
+        }
+
+        [TestMethod]
+        public void CreatePoint2()
+        {
+            var a = new Point(0, 0);
+        }
+
+        [TestMethod]
+        public void CreateSquare_Test() 
+        { 
+            var sq = new Square(0, 0);
+        }
+
+        [TestMethod]
+        public void CreateRectangle_Test()
+        {
+            var rec = new Rectangle(0, 0);
+        }
+
+        [TestMethod]
+        public void CreateRomb_Test()
+        {
+            var sq = new Romb(0, 0);
+        }
+
+        [TestMethod]
+        public void movePointRight_Test()
+        {
+            var a = new Point(0, 0);
+            a.movePoint(2, 0);
+            double x = a.X;
+            double y = a.Y;
+            Assert.AreEqual(2, x);
+            Assert.AreEqual(0, y);
+        }
+
+        [TestMethod]
+        public void movePointLeft_Test()
+        {
+            var a = new Point(0, 0);
+            a.movePoint(-2, 0);
+            double x = a.X;
+            double y = a.Y;
+            Assert.AreEqual(-2, x);
+            Assert.AreEqual(0, y);
+        }
+
+        [TestMethod]
+        public void movePointTop_Test()
+        {
+            var a = new Point(0, 0);
+            a.movePoint(0, 2);
+            double x = a.X;
+            double y = a.Y;
+            Assert.AreEqual(0, x);
+            Assert.AreEqual(2, y);
+        }
+
+        [TestMethod]
+        public void movePointBottom_Test()
+        {
+            var a = new Point(0, 0);
+            a.movePoint(0, -2);
+            double x = a.X;
+            double y = a.Y;
+            Assert.AreEqual(0, x);
+            Assert.AreEqual(-2, y);
+        }
+
+        [TestMethod]
+        public void moveSquareRight_Test()
+        {
+            var sq = new Square(0, 0);
+            sq.moveSquare(2, 0);
+            double ax = sq.AX;
+            double ay = sq.AY;
+            double bx = sq.BX;
+            double by = sq.BY;
+            double cx = sq.CX;
+            double cy = sq.CY;
+            double dx = sq.DX;
+            double dy = sq.DY;
+            Assert.AreEqual(2, ax);
+            Assert.AreEqual(0, ay);
+            Assert.AreEqual(2, bx);
+            Assert.AreEqual(5, by);
+            Assert.AreEqual(7, cx);
+            Assert.AreEqual(5, cy);
+            Assert.AreEqual(7, dx);
+            Assert.AreEqual(0, dy);
+        }
+
+        [TestMethod]
+        public void moveSquareLeft_Test()
+        {
+            var sq = new Square(0, 0);
+            sq.moveSquare(-2, 0);
+            double ax = sq.AX;
+            double ay = sq.AY;
+            double bx = sq.BX;
+            double by = sq.BY;
+            double cx = sq.CX;
+            double cy = sq.CY;
+            double dx = sq.DX;
+            double dy = sq.DY;
+            Assert.AreEqual(-2, ax);
+            Assert.AreEqual(0, ay);
+            Assert.AreEqual(-2, bx);
+            Assert.AreEqual(5, by);
+            Assert.AreEqual(3, cx);
+            Assert.AreEqual(5, cy);
+            Assert.AreEqual(3, dx);
+            Assert.AreEqual(0, dy);
+        }
+
+        [TestMethod]
+        public void moveSquareTop_Test()
+        {
+            var sq = new Square(0, 0);
+            sq.moveSquare(0, 2);
+            double ax = sq.AX;
+            double ay = sq.AY;
+            double bx = sq.BX;
+            double by = sq.BY;
+            double cx = sq.CX;
+            double cy = sq.CY;
+            double dx = sq.DX;
+            double dy = sq.DY;
+            Assert.AreEqual(0, ax);
+            Assert.AreEqual(2, ay);
+            Assert.AreEqual(0, bx);
+            Assert.AreEqual(7, by);
+            Assert.AreEqual(5, cx);
+            Assert.AreEqual(7, cy);
+            Assert.AreEqual(5, dx);
+            Assert.AreEqual(2, dy);
+        }
+
+        [TestMethod]
+        public void moveSquareBottom_Test()
+        {
+            var sq = new Square(0, 0);
+            sq.moveSquare(0, -2);
+            double ax = sq.AX;
+            double ay = sq.AY;
+            double bx = sq.BX;
+            double by = sq.BY;
+            double cx = sq.CX;
+            double cy = sq.CY;
+            double dx = sq.DX;
+            double dy = sq.DY;
+            Assert.AreEqual(0, ax);
+            Assert.AreEqual(-2, ay);
+            Assert.AreEqual(0, bx);
+            Assert.AreEqual(3, by);
+            Assert.AreEqual(5, cx);
+            Assert.AreEqual(3, cy);
+            Assert.AreEqual(5, dx);
+            Assert.AreEqual(-2, dy);
+        }
+    }
+}
